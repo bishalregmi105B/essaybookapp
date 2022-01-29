c41b5c41dcc20a87f4dce03c06806432fc12b557
bishalregmi105B <89974678+bishalregmi105B@users.noreply.github.com>
2022-01-29 18:27:30 +0545

    Nothing
    Code

---
+++ essays.py
@@ -0,0 +1,241 @@
+import os
+path='./essays/'
+import webbrowser
+new = 2 # open in a new tab, if possible
+
+##################Peace################################
+impofpeace= open(path+'peace.ash', 'r',encoding='utf-8')
+imppeace=impofpeace.read()
+
+##################Prayer################################
+impofprayer= open(path+'prayer.ash', 'r',encoding='utf-8')
+impprayer=impofprayer.read()
+
+##################tolerance################################
+impoftolerance= open(path+'tolerance.ash', 'r',encoding='utf-8')
+imptolerance=impoftolerance.read()
+
+##################Globalization################################
+globalize= open(path+'globalization.ash', 'r',encoding='utf-8')
+globalization=globalize.read()
+
+##################Patriotism################################
+impofpatriotism= open(path+'patriotism.ash', 'r',encoding='utf-8')
+patriotism=impofpatriotism.read()
+
+##################Travel################################
+Travel= open(path+'travelling.ash', 'r',encoding='utf-8')
+travelling=Travel.read()
+
+##################socialmedia################################
+useSocialmedia= open(path+'socialmedia.ash', 'r',encoding='utf-8')
+socialmedia=useSocialmedia.read()
+
+##################starvation################################
+essaystarvation= open(path+'starvation.ash', 'r',encoding='utf-8')
+starvation=essaystarvation.read()
+
+##################mobilephone################################
+impmobilephone= open(path+'mobilephone.ash', 'r',encoding='utf-8')
+mobilephone=impmobilephone.read()
+
+##################hinduism################################
+esshinduism= open(path+'hinduism.ash', 'r',encoding='utf-8')
+hinduism=esshinduism.read()
+
+##################telivision################################
+demtelivision= open(path+'telivision.ash', 'r',encoding='utf-8')
+telivision=demtelivision.read()
+
+##################riversinnepal################################
+impriversinnepal= open(path+'riversinnepal.ash', 'r',encoding='utf-8')
+riversinnepal=impriversinnepal.read()
+
+##################Imp of Technology################################
+imptechnology= open(path+'importanceoftechnology.ash', 'r',encoding='utf-8')
+impoftechnology=imptechnology.read()
+
+##################Time management################################
+time= open(path+'timemanagement.ash', 'r',encoding='utf-8')
+timemanagement=time.read()
+
+##################Poverty################################
+ypoverty= open(path+'poverty.ash', 'r',encoding='utf-8')
+poverty=ypoverty.read()
+
+##################pleasureofreading################################
+reading= open(path+'pleasureofreading.ash', 'r',encoding='utf-8')
+pleasureofreading=reading.read()
+
+##################honesty################################
+ihonesty= open(path+'honesty.ash', 'r',encoding='utf-8')
+honesty=ihonesty.read()
+
+##################knowledgeispower################################
+gknowledgeispower= open(path+'knowledgeispower.ash', 'r',encoding='utf-8')
+knowledgeispower=gknowledgeispower.read()
+
+##################impofhealtheducation################################
+healtheducation= open(path+'impofhealtheducation.ash', 'r',encoding='utf-8')
+impofhealtheducation=healtheducation.read()
+
+##################missuseofscience################################
+science= open(path+'missuseofscience.ash', 'r',encoding='utf-8')
+missuseofscience=science.read()
+
+##################cybercrime################################
+ucybercrime= open(path+'cybercrime.ash', 'r',encoding='utf-8')
+cybercrime=ucybercrime.read()
+
+##################corruption################################
+corruptione= open(path+'corruption.ash', 'r',encoding='utf-8')
+corruption=corruptione.read()
+
+##################lustforwealth################################
+wealth= open(path+'lustforwealth.ash', 'r',encoding='utf-8')
+lustforwealth=wealth.read()
+
+##################facetofacecommunication################################
+efacetofacecommunication= open(path+'facetofacecommunication.ash', 'r',encoding='utf-8')
+facetofacecommunication=efacetofacecommunication.read()
+
+##################discipline################################
+impdiscipline= open(path+'discipline.ash', 'r',encoding='utf-8')
+discipline=impdiscipline.read()
+
+##################hobby################################
+myhobby= open(path+'myhobby.ash', 'r',encoding='utf-8')
+hobby=myhobby.read()
+
+#sachyaunu xa
+##################myfamily################################
+imyfamily= open(path+'myfamily.ash', 'r',encoding='utf-8')
+myfamily=imyfamily.read()
+
+##################female education################################
+ifemaleeducation= open(path+'femaleeducation.ash', 'r',encoding='utf-8')
+femaleeducation=ifemaleeducation.read()
+
+##################valueoftime################################
+ivalueoftime= open(path+'valueoftime.ash', 'r',encoding='utf-8')
+valueoftime=ivalueoftime.read()
+
+##################Dashain################################
+gDashain= open(path+'Dashain.ash', 'r',encoding='utf-8')
+Dashain=gDashain.read()
+
+##################humannature################################
+ihumannature= open(path+'humannature.ash', 'r',encoding='utf-8')
+humannature=ihumannature.read()
+
+##################myschool################################
+Myschool= open(path+'myschool.ash', 'r',encoding='utf-8')
+myschool=Myschool.read()
+
+##################florencenightangle################################
+Florencenightangle= open(path+'florencenightangle.ash', 'r',encoding='utf-8')
+florencenightangle=Florencenightangle.read()
+
+##################worldwarI################################
+worldwari= open(path+'worldwarI.ash', 'r',encoding='utf-8')
+worldwarI=worldwari.read()
+
+##################Internet################################
+Internet= open(path+'internet.ash', 'r',encoding='utf-8')
+internet=Internet.read()
+
+##################Nepal################################
+Nepal= open(path+'nepal.ash', 'r',encoding='utf-8')
+nepal=esshinduism.read()
+
+##################impofcomputer################################
+Computer= open(path+'computer.ash', 'r',encoding='utf-8')
+computer=Computer.read()
+
+##################myvillage################################
+imyvillage= open(path+'myvillage.ash', 'r',encoding='utf-8')
+myvillage=imyvillage.read()
+
+##################environment################################
+ienvironment= open(path+'environment.ash', 'r',encoding='utf-8')
+environment=ienvironment.read()
+
+##################impeducation################################
+Impeducation= open(path+'impeducation.ash', 'r',encoding='utf-8')
+impeducation=Impeducation.read()
+
+##################dowry################################
+ydowry= open(path+'dowry.ash', 'r',encoding='utf-8')
+dowry=ydowry.read()
+
+##################superstation################################
+Superstation= open(path+'superstation.ash', 'r',encoding='utf-8')
+superstation=Superstation.read()
+
+##################politics################################
+ipolitics= open(path+'politics.ash', 'r',encoding='utf-8')
+politics=ipolitics.read()
+
+##################Socialservics################################
+gSocialservics= open(path+'Socialservics.ash', 'r',encoding='utf-8')
+Socialservics=gSocialservics.read()
+
+##################Gautambuddha################################
+hGautambuddha= open(path+'Gautambuddha.ash', 'r',encoding='utf-8')
+Gautambuddha=hGautambuddha.read()
+
+##################fashain################################
+Fashain= open(path+'fashain.ash', 'r',encoding='utf-8')
+fashain=Fashain.read()
+
+##################tourisminnepal################################
+utourisminnepal= open(path+'tourisminnepal.ash', 'r',encoding='utf-8')
+tourisminnepal=utourisminnepal.read()
+
+##################Thecow################################
+eThecow= open(path+'Thecow.ash', 'r',encoding='utf-8')
+Thecow=eThecow.read()
+
+##################Healthiswealth################################
+wHealthiswealth= open(path+'Healthiswealth.ash', 'r',encoding='utf-8')
+Healthiswealth=wHealthiswealth.read()
+
+##################game################################
+Game= open(path+'game.ash', 'r',encoding='utf-8')
+game=Game.read()
+
+##################discipline################################
+Newspasper= open(path+'newspaper.ash', 'r',encoding='utf-8')
+newspaper=Newspasper.read()
+
+##################hobby################################
+Love= open(path+'love.ash', 'r',encoding='utf-8')
+love=Love.read()
+
+##################globalwarming################################
+Fglobalwarming= open(path+'globalwarming.ash', 'r',encoding='utf-8')
+globalwarming=Fglobalwarming.read()
+
+##################population################################
+upopulation= open(path+'population.ash', 'r',encoding='utf-8')
+population=upopulation.read()
+
+##################democracy################################
+edemocracy= open(path+'democracy.ash', 'r',encoding='utf-8')
+democracy=edemocracy.read()
+
+##################Religions################################
+wReligions= open(path+'Religions.ash', 'r',encoding='utf-8')
+Religion=wReligions.read()
+
+##################deforestation################################
+Gdeforestation= open(path+'deforestation.ash', 'r',encoding='utf-8')
+deforestation=Gdeforestation.read()
+
+##################covi19################################
+covid= open(path+'covid19.ash', 'r',encoding='utf-8')
+covid19=covid.read()
+
+##################myschool################################
+cmyschool= open(path+'myschool.ash', 'r',encoding='utf-8')
+myschool=cmyschool.read()
